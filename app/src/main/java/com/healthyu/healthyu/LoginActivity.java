9221b1b4832066499086195580ad3678d6e75e2c
OnDEK <dkinney94@gmail.com>
2017-04-04 15:04:28 -0400

    Hide Keyboard and Check Password Match
    Hides keyboard on focus change in login and register activities, shows
    indicator of password match on register activity.

--- app/src/main/java/com/healthyu/healthyu/LoginActivity.java
+++ app/src/main/java/com/healthyu/healthyu/LoginActivity.java
@@ -4,20 +4,51 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 /**
  * Created by Marina on 3/9/2017.
  */
 
 public class LoginActivity extends Activity {
+
+    TextView usernameTV, passwordTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameTV = (TextView) findViewById(R.id.loginUsername);
+        passwordTV = (TextView) findViewById(R.id.loginPassword);
+
+        usernameTV.setOnFocusChangeListener(hideKeyboardListener);
+        passwordTV.setOnFocusChangeListener(hideKeyboardListener);
     }
+
+
+    View.OnFocusChangeListener hideKeyboardListener = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (!hasFocus) {
+                hideKeyboard(v);
+            }
+        }
+    };
 
     public void onLoginRequest(View v){
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
+
+        /**
+         * Check session preferences  against the strings inside the password and username TextViews
+         * startActivity if valid, throw a dialog box if invalid
+         */
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
--- app/src/main/java/com/healthyu/healthyu/RegisterActivity.java
+++ app/src/main/java/com/healthyu/healthyu/RegisterActivity.java
@@ -2,15 +2,80 @@ package com.healthyu.healthyu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Marina on 3/9/2017.
  */
 
 public class RegisterActivity extends Activity {
+
+    TextView usernameTV, passwordTV, passwordConfirmTV;
+    ImageView registerPasswordConfirmImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        View.OnFocusChangeListener hideKeyboardListener = new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        };
+
+        usernameTV = (TextView) findViewById(R.id.registerUsername);
+        passwordTV = (TextView) findViewById(R.id.registerPassword);
+        passwordConfirmTV = (TextView) findViewById(R.id.registerPasswordConfirm);
+        registerPasswordConfirmImage = (ImageView) findViewById(R.id.registerPasswordConfirmImage);
+
+        usernameTV.setOnFocusChangeListener(hideKeyboardListener);
+        passwordTV.setOnFocusChangeListener(hideKeyboardListener);
+        passwordConfirmTV.setOnFocusChangeListener(hideKeyboardListener);
+
+        passwordConfirmTV.addTextChangedListener(new TextWatcher() {
+            String currentString;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                currentString = passwordConfirmTV.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(currentString.equals(passwordTV.getText().toString())) {
+                    registerPasswordConfirmImage.setImageResource(R.drawable.checkmark);
+                }else {
+                    registerPasswordConfirmImage.setImageResource(R.drawable.exclamation_red);
+                }
+            }
+        });
+    }
+
+    public void onREgisterClick(View v) {
+        String username, password, passwordConfirm;
+
+        /**
+         * Add the password and username to sessionpreferences
+         */
+
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
